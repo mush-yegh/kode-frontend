b6935e50773099d9b26da5ff5fc35e33aa8ce4f1
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-11-03 19:55:47 +0400

    Add Profile component with corresponding route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 import HomeScreen from "../HomeScreen";
+import Profile from "./../Profile";
 import { Container } from "semantic-ui-react";
 import styles from "./index.module.scss";
 
@@ -10,6 +11,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <HomeScreen />
+          </Route>
+          <Route path="/profile">
+            <Profile />
           </Route>
           <Redirect to="/" />
         </Switch>
---
+++ src/components/Profile/index.js
@@ -0,0 +1,7 @@
+import styles from "./index.module.scss";
+
+function Profile() {
+  return <div id={styles.profile}>Profile Page</div>;
+}
+
+export default Profile;
