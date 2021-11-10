21a642e664ed04e6f043e1268897006d7ddb5408
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-11-10 17:22:17 +0400

    Optimize and add some explanation comments to Workers

--- src/components/Workers/index.js
+++ src/components/Workers/index.js
@@ -6,22 +6,25 @@ import Worker from "../Worker";
 import { Divider } from "semantic-ui-react";
 import styles from "./index.module.scss";
 
-const Workers = ({ workers, isBirthDateVisible }) => {
-  const currentYear = getYear(Date.now());
+function isBirthdayPassed(worker) {
+  const workerBirthDate = new Date(worker.birthday);
+  const bDayInCurrentYear = setYear(workerBirthDate, getYear(Date.now()));
+  return isPast(endOfDay(bDayInCurrentYear));
+}
 
-  const firstNextYearBdayWorker = workers.find((w) => {
-    if (!w.isInSelectedDep) {
-      return false;
-    }
-    const bDayInCurrenYear = setYear(new Date(w.birthday), getYear(Date.now()));
-    return isPast(endOfDay(bDayInCurrenYear));
-  });
+// Returns the first found worker whose birthday has already passed this year
+// If nothing was found - returns undefined
+function getFirstNextYearBdayWorker(workers) {
+  return workers.find(isBirthdayPassed);
+}
 
+const Workers = ({ workers, isBirthDayVisible, isLoading }) => {
   const { id: scrollToId } = useSelector(
     (state) => state.profile,
     shallowEqual
   );
 
+  const scrollToItem = useRef(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -34,28 +37,29 @@ const Workers = ({ workers, isBirthDateVisible }) => {
     }
   }, [scrollToId, dispatch]);
 
-  const scrollToItem = useRef(null);
-  const isYearLineVisible = isBirthDateVisible && firstNextYearBdayWorker;
+  const firstNextYearBdayWorker = getFirstNextYearBdayWorker(workers);
+
+  // Year divider is visible when chosen sorting algorithm is by closest birthday
+  // and there is at least one worker whose upcoming birthday is next year
+  const isYearLineVisible = isBirthDayVisible && firstNextYearBdayWorker;
 
   return (
     <div id={styles.workers}>
-      {workers
-        .filter((worker) => worker.isInSelectedDep && worker.isInSearch)
-        .map((worker) => {
-          return (
-            <div
-              key={worker.id}
-              ref={scrollToId && scrollToId === worker.id ? scrollToItem : null}
-            >
-              {isYearLineVisible && firstNextYearBdayWorker.id === worker.id && (
-                <Divider className={styles.hr_line} horizontal>
-                  {currentYear + 1}
-                </Divider>
-              )}
-              <Worker worker={{ ...worker, isBirthDateVisible }} />
-            </div>
-          );
-        })}
+      {workers.map((worker) => {
+        return (
+          <div
+            key={worker.id}
+            ref={scrollToId && scrollToId === worker.id ? scrollToItem : null}
+          >
+            {isYearLineVisible && firstNextYearBdayWorker.id === worker.id && (
+              <Divider className={styles.hr_line} horizontal>
+                {getYear(Date.now()) + 1}
+              </Divider>
+            )}
+            <Worker worker={worker} isBirthDayVisible={isBirthDayVisible} />
+          </div>
+        );
+      })}
     </div>
   );
 };
